923a481d7e750af602ee4bdae9f91ca0f015197d
Ahmet Altun <104174249+ahmetaltunfatih@users.noreply.github.com>
2025-09-09 10:38:55 +0200

    run_inp_prep.py

--- examples/ledip-inputs/run_inp_prep.py
+++ examples/ledip-inputs/run_inp_prep.py
@@ -1,3 +1,34 @@
+########################################################################################
+#                                                                                      #
+#                           LEDAW - LED Analysis Wizard                                #
+#                                    Ledip Module                                      #
+#                                                                                      #
+#                 Automate Local Energy Decomposition Analysis                         #
+#                           Using Data in ORCA Outputs                                 #
+#                                                                                      #
+#                            written by Ahmet Altun                                    #
+#                    Max-Planck-Institut für Kohlenforshung                            #
+#                Department of Molecular Theory and Spectroscopy                       #
+#                                                                                      #
+#                                                                                      #
+#                                 Citation                                             #
+#               If you use any part of this code, in addition to                       #
+#               original LED, CPS, and CBS studies, please cite:                       #
+#                                                                                      #
+#                 1) https://pubs.acs.org/doi/full/10.1021/acs.jcim.5c01561            #
+#                    (J. Chem. Inf. Model. 65/17, 2025, 8917–8923)                     #
+#                 2) https://github.com/ahmetaltunfatih/LEDAW                          #
+#                 3) https://doi.org/10.1002/anie.202421922                            #
+#                    (Angew. Chemie. Int. Ed. 64/12, 2025, e202421922)                 #
+#                                                                                      #
+#                                    License                                           #
+#                             Free for academic use.                                   #
+#           For commercial use or redistribution, contact the author.                  #
+#             The author provides this code as-is, without warranty.                   #
+#                                                                                      #
+########################################################################################
+
+
 import sys
 sys.path.insert(0,"../..")
 from ledip_package import *
